1cd88805de2c19a6bfccc66779e3e6aca543c88a
Javier Gularte <133526577+eddie-gs@users.noreply.github.com>
2023-08-05 08:59:36 -0300

    Solucion stopPropagation
    Solución de que el botón active también el alert del div mediante el método stopPropagation

--- eventos.js
+++ eventos.js
@@ -1,5 +1,10 @@
 const Div = document.getElementById("div-button")
+const Button = document.getElementById("button")
 
 Div.addEventListener("click", function() {
-    this.onclick(alert('Hola! Soy el div'))
+    this.onclick(alert('Hola! Soy el div'))    
+})
+
+Button.addEventListener("click", (event) => {
+    event.stopPropagation();
 })
